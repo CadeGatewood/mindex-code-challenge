d345beb9bc94829ea845ef1ab9af4ab141af4984
mindex <dev@mindex.com>
2024-11-01 22:25:22 -0500

    import cleanup

--- src/main/java/com/mindex/challenge/controller/GlobalAdviceController.java
+++ src/main/java/com/mindex/challenge/controller/GlobalAdviceController.java
@@ -1,17 +1,12 @@
 package com.mindex.challenge.controller;
 
+import com.mindex.challenge.data.ErrorResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import com.mindex.challenge.data.ErrorResponse;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @ControllerAdvice
 public class GlobalAdviceController {
